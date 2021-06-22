a875fa65e73c2ef393a936d888b4af0d948be4de
Andrew <tankist.scratch.p@gmail.com>
2021-06-22 21:07:30 +0300

    Another way to extend PATH...

--- src/py/2.0.0/__init__.py
+++ src/py/2.0.0/__init__.py
@@ -2,11 +2,14 @@ import os
 import os.path
 
 path = os.path.abspath(os.path.dirname(__file__))
-if path not in os.getenv("PATH"):
-    os.putenv("PATH", path + os.pathsep + os.getenv("PATH"))
+if path not in os.environ["PATH"]:
+    os.environ["PATH"] = path + os.pathsep + os.getenv("PATH")
+
+try:
+    os.add_dll_directory(path)
+except AttributeError:
+    pass
 
 del path
 del os.path
 del os
-
-
