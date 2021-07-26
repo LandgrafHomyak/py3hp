1b476a1c2071662c0844737bff20b10c45ee99a3
Andrew <tankist.scratch.p@gmail.com>
2021-07-26 22:48:12 +0300

    [PP] setup script classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from setuptools import setup
 setup(
     name="pyhp-python",
     packages=["pyhp"],
-    package_data={"pyhp": ["py.typed", "__init__.pyi"]},
+    package_data={"pyhp": ["*.pyi"]},
     package_dir={"pyhp": "pkg"},
-    version="0.0.0"
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Interpreters",
+        "Topic :: Text Processing :: Markup",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Typing :: Typed"
+    ],
+    version="0.0.0rc0"
 )
